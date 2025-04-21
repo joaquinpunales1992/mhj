2fabd8c57799f7db1feeab507e96948799d93909
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-04-21 15:17:14 -0500

    allow simple email registration

--- front/views.py
+++ front/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from inventory.models import Property
+from django.core.mail import EmailMessage
 
 def display_home(request):
     """
@@ -22,29 +23,55 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from django.http import Http404
 
 @csrf_exempt
 def send_booking_confirmation(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        user_email = data.get('email')
+
+        user_email = data.get('user_email')
         property_url = data.get('url')
 
         # Render the email template
         html_message = render_to_string('emails/booking_confirmation.html', {'property_url': property_url})
-        plain_message = strip_tags(html_message)
 
-        send_mail(
-            'Your House in Japan',
-            plain_message,
-            'noreply@myhouseinjapan.com',
-            [user_email, 'joaquinpunales@gmail.com'],
-            html_message=html_message,
+        email = EmailMessage(
+            subject='Your House in Japan - Booking Confirmation',
+            body=html_message,
+            from_email='noreply@myhouseinjapan.com',
+            to=[user_email], # Add at least one recipient in the 'to' field
+            bcc=['joaquinpunales@gmail.com'],
+            reply_to=['noreply@myhouseinjapan.com']
         )
+
+        email.content_subtype = 'html'
+        try:
+            email.send()
+        except Exception as e:
+            print(f"Error sending email: {e}")
+
         return JsonResponse({'message': 'Email sent'})
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 
 def contact_seller(request, pk):
     property = Property.objects.filter(pk=pk).first()
-    return render(request, 'contact_seller.html', context={'property': property})
+    user_email = None
+    
+    try:
+        if getattr(request.user, 'email', None) is not None:
+            user_email = request.user.email
+    except AttributeError:
+        pass
+
+    try:
+        if 'email' in request.COOKIES:
+            user_email = request.COOKIES['email']
+    except AttributeError:
+        pass
+
+    if not user_email:
+        return JsonResponse({'error': 'Forbidden request'}, status=403)
+    
+    return render(request, 'contact_seller.html', context={'property': property, 'user_email': user_email})
--- membership/views.py
+++ membership/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def show_authenticate_page(request, pk):
-    return render(request, 'authentication_page.html')
+    return render(request, 'authentication_page.html', context={'property_pk': pk})
+
+
+def register_via_email(request, pk):
+    email = request.POST.get('email')
+    if email:
+        response = redirect(reverse('contact_seller', args=[pk]))
+        response.set_cookie('email', email)
+        return response
 
