d2613f72cbb179873cadf0378e55ee4cf475b1f7
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-04-27 11:57:07 -0500

    fix sso

---
+++ membership/adapter.py
@@ -0,0 +1,12 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def populate_user(self, request, sociallogin, data):
+        user = super().populate_user(request, sociallogin, data)
+        
+        if not user.username: 
+            user.username = user.email
+
+        return user
