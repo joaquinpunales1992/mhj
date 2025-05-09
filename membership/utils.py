2b38815286644db29de016e92d152660cdf746a1
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-05-09 17:46:54 -0500

    tweaks

--- membership/utils.py
+++ membership/utils.py
@@ -4,7 +4,7 @@ from django.core.mail import EmailMessage
 def notify_user_registered_via_email(new_user_email):
 
     email = EmailMessage(
-        subject='Your House in Japan - NEW USER REGISTERD VIA EMAIL',
+        subject='Your Akiya in Japan - NEW USER REGISTERD VIA EMAIL',
         body=f"New User Registered via Emial: {new_user_email}",
         from_email='noreply@myhouseinjapan.com',
         to=['joaquinpunales@gmail.com'],
