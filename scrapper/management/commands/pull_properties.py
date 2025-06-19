43e9c4f0509a01a5ef5e6de79e7f6a0fa31b31ea
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-06-19 14:10:00 -0500

    tweak command

--- scrapper/management/commands/pull_properties.py
+++ scrapper/management/commands/pull_properties.py
@@ -4,14 +4,31 @@ from scrapper.constants import LIFULL_HOMES_BASE_URL, LIFULL_HOMES_REGION_LIST
 
 class Command(BaseCommand):
     help = 'Pull properties from the specified website'
+    
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--region',
+            type=str,
+            help='Specify the region to pull properties from',
+            required=False
+        )
 
     def handle(self, *args, **kwargs):
         try:
-            for region in LIFULL_HOMES_REGION_LIST:
+            region = kwargs.get('region')
+            import pdb;pdb.set_trace()
+            if region:
                 page_from = 1
                 page_to = 50
                 listing_url = f"{LIFULL_HOMES_BASE_URL}/kodate/chuko/{region}/list/"
                 pull_properties(listing_url=listing_url, page_from=page_from, page_to=page_to)
                 self.stdout.write(self.style.SUCCESS(f'Successfully pulled properties - {region}'))
+            else:
+                for region in LIFULL_HOMES_REGION_LIST:
+                    page_from = 1
+                    page_to = 50
+                    listing_url = f"{LIFULL_HOMES_BASE_URL}/kodate/chuko/{region}/list/"
+                    pull_properties(listing_url=listing_url, page_from=page_from, page_to=page_to)
+                    self.stdout.write(self.style.SUCCESS(f'Successfully pulled properties - {region}'))
         except Exception as e:
             self.stderr.write(self.style.ERROR(f'Error pulling properties: {e}'))
