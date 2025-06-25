5975ebf982750872ed09d974cade9eb844b1a019
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-06-25 15:25:18 -0500

    wip group posting

--- social/social_bot.py
+++ social/social_bot.py
@@ -275,8 +275,7 @@ def post_on_facebook_batch(price_limit: int, batch_size: int):
         'property_url',
         flat=True
     )
-
-    properties_to_post_facebook = Property.objects.filter(images__isnull=False, price__lte=price_limit, featured=True).exclude(url__in=facebook_posted_urls).order_by('price').distinct()[:batch_size]
+    properties_to_post_facebook = Property.objects.filter(images__isnull=False, price__lte=price_limit).exclude(url__in=facebook_posted_urls).order_by('price').distinct()[:batch_size]
     for property in properties_to_post_facebook:
         try:
             post_to_facebook(property=property, use_ai_caption=USE_AI_CAPTION)
@@ -300,3 +299,21 @@ def post_on_instagram_batch(price_limit: int, batch_size: int):
         except Exception as e:
             print(f"Error posting property {property.id}: {e}")
             continue
+
+
+
+
+GROUP_ID = ''
+
+def post_on_facebook_group():
+    post_url = f"https://graph.facebook.com/v19.0/{GROUP_ID}/feed"
+    payload = {
+        "message": "Check out this new property in Kyoto!",
+        "access_token": get_fresh_token()
+    }
+
+    response = requests.post(post_url, data=payload)
+    if response.status_code == 200:
+        print("✅ Successfully posted to Facebook Group.")
+    else:
+        print(f"❌ Failed to post to Facebook Group: {response.json()}")
