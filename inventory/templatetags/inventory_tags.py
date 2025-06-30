7ee2b4163c42a1e5b703f180c2babf7e8e806677
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-06-30 14:35:45 -0500

    add related properties

---
+++ inventory/templatetags/inventory_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from inventory.models import Property
+from inventory.constants import MAX_RELATED_PROPERTIES
+from django.db.models import QuerySet
+
+register = template.Library()
+
+
+@register.simple_tag
+def find_related_properties(property_pk: int) -> QuerySet:
+    return Property.objects.filter(location__icontains=Property.objects.get(pk=property_pk).get_location_for_front(), premium=False).exclude(pk=property_pk).order_by('price')[:MAX_RELATED_PROPERTIES]
