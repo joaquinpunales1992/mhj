5d57e5993729181c07be1bc89726ae2bdfc7d151
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-06-30 13:49:36 -0500

    fix wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-application = get_wsgi_application()n()
+application = get_wsgi_application()
