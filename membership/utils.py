d0100525b55d5c7b75650144afd43b4a2b10b7a2
joaquinp1992 <joaquinpunales@gmail.com>
2025-08-04 13:34:52 -0500

    log users rexpressing interest

--- membership/utils.py
+++ membership/utils.py
@@ -1,10 +1,10 @@
 from django.core.mail import EmailMessage
 
 
-def notify_social_token_expired(message=None):
+def notification_email(subject: str, body: str):
     email = EmailMessage(
-        subject="Your Akiya in Japan - SOCIAL TOKEN EXPIRED",
-        body=message,
+        subject=subject,
+        body=body,
         from_email="hello@myakiyainjapan.com",
         to=["joaquinpunales@gmail.com"],
         reply_to=["hello@myakiyainjapan.com"],
@@ -16,18 +16,14 @@ def notify_social_token_expired(message=None):
     except Exception as e:
         print(f"Error sending email: {e}")
 
+
+def notify_social_token_expired(message=None):
+    return notification_email(subject="Your Akiya in Japan - SOCIAL TOKEN EXPIRED", body=message)
+    
 
 def notify_user_registered_via_email(new_user_email):
-    email = EmailMessage(
-        subject="Your Akiya in Japan - NEW USER REGISTERD VIA EMAIL",
-        body=f"New User Registered via Emial: {new_user_email}",
-        from_email="hello@myakiyainjapan.com",
-        to=["joaquinpunales@gmail.com"],
-        reply_to=["hello@myakiyainjapan.com"],
-    )
+    return notification_email(subject="Your Akiya in Japan - NEW USER REGISTERD VIA EMAIL", body=f"New User Registered via Emial: {new_user_email}")
+    
 
-    email.content_subtype = "html"
-    try:
-        email.send()
-    except Exception as e:
-        print(f"Error sending email: {e}")
+async def notify_user_expressed_interest(email: str):
+    notification_email(subject="USER EXPRESSED INTERESTS", body=f"USER EXPRESSED INTEREST: {email}")
