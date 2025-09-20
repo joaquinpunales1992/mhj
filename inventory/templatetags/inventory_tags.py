e8d7705745d701a11fb2b43c7c751a0706280c39
joaquinp1992 <joaquinpunales@gmail.com>
2025-09-20 13:07:46 -0500

    show random related houses

--- inventory/templatetags/inventory_tags.py
+++ inventory/templatetags/inventory_tags.py
@@ -9,18 +9,18 @@ register = template.Library()
 
 @register.simple_tag
 def find_related_properties(property_pk: int) -> QuerySet:
-    return (
-        Property.objects.filter(
-            location__icontains=Property.objects.get(
-                pk=property_pk
-            ).get_location_for_front(),
-            premium=False,
-            images__isnull=False,
-        )
-        .exclude(pk=property_pk)
-        .order_by("price")
-        .distinct()[:MAX_RELATED_PROPERTIES]
-    )
+    queryset = Property.objects.filter(
+        location__icontains=Property.objects.get(
+            pk=property_pk
+        ).get_location_for_front(),
+        premium=False,
+        images__isnull=False,
+    ).exclude(pk=property_pk).distinct()
+    # Get all matching pks, shuffle, and slice
+    pks = list(queryset.values_list('pk', flat=True))
+    random.shuffle(pks)
+    selected_pks = pks[:MAX_RELATED_PROPERTIES]
+    return Property.objects.filter(pk__in=selected_pks)
 
 
 @register.filter
